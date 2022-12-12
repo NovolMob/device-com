15fbd30d4c664e9301718ae1f6f0fed5fe7885d6
NovolMob <60779683+NovolMob@users.noreply.github.com>
2022-12-12 19:59:00 +0300

    init: backend

---
+++ backend/build.gradle.kts
@@ -0,0 +1,53 @@
+val serialization_version: String by project
+val exposed_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val ktor_version: String by project
+
+plugins {
+    application
+    java
+    `java-library`
+    kotlin("jvm") version "1.7.22"
+    id("io.ktor.plugin") version "2.1.3"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.22"
+}
+
+group = "ru.novolmob"
+version = "0.0.1"
+application {
+    mainClass.set("MainKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":database"))
+    implementation(project(":backend-api"))
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serialization_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+    implementation("io.arrow-kt:arrow-core:1.0.1")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-sessions-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-host-common-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-status-pages-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-resources:$ktor_version")
+    implementation("io.ktor:ktor-server-cors-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    testImplementation(kotlin("test"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@ rootProject.name = "bd-practice"
 include("exposed-database")
 include("backend-api")
 include("core")
+include("backend")
