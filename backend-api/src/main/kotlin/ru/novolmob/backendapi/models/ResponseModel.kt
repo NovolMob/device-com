74deadac2ce84d47a76349544296f08aee3bc64b
NovolMob <kiria.shikov@mail.ru>
2022-12-12 15:21:42 +0300

    feat: added default value for response model constructor

--- backend-api/src/main/kotlin/ru/novolmob/backendapi/models/ResponseModel.kt
+++ backend-api/src/main/kotlin/ru/novolmob/backendapi/models/ResponseModel.kt
@@ -3,6 +3,6 @@ package ru.novolmob.backendapi.models
 import ru.novolmob.backendapi.exceptions.BackendException
 
 data class ResponseModel<T>(
-    val data: T?,
-    val exception: BackendException?
+    val data: T? = null,
+    val exception: BackendException? = null
 )
