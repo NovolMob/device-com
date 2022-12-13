ed291636307f4d18b5b209ecb70faa18dd04f5ae
NovolMob <kiria.shikov@mail.ru>
2022-12-13 18:19:03 +0300

    feat: added some codes

--- backend-api/src/main/kotlin/ru/novolmob/backendapi/exceptions/BackendExceptionCode.kt
+++ backend-api/src/main/kotlin/ru/novolmob/backendapi/exceptions/BackendExceptionCode.kt
@@ -1,5 +1,7 @@
 package ru.novolmob.backendapi.exceptions
 
 enum class BackendExceptionCode(val httpCode: Int, val description: String) {
-    UNKNOWN(500, "")
+    UNKNOWN(500, ""),
+    BAD_REQUEST(400, ""),
+    NOT_FOUND(404, "");
 }
