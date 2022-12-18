2a90e1f2113b587857d60adfb7c7e62affacdd93
NovolMob <kiria.shikov@mail.ru>
2022-12-18 16:33:21 +0300

    feat: added getFull method in order status repository interface

--- backend-api/src/main/kotlin/ru/novolmob/backendapi/repositories/IOrderStatusRepository.kt
+++ backend-api/src/main/kotlin/ru/novolmob/backendapi/repositories/IOrderStatusRepository.kt
@@ -1,8 +1,14 @@
 package ru.novolmob.backendapi.repositories
 
+import arrow.core.Either
+import ru.novolmob.backendapi.exceptions.BackendException
 import ru.novolmob.backendapi.models.OrderStatusCreateModel
+import ru.novolmob.backendapi.models.OrderStatusFullModel
 import ru.novolmob.backendapi.models.OrderStatusModel
 import ru.novolmob.backendapi.models.OrderStatusUpdateModel
+import ru.novolmob.database.models.Language
 import ru.novolmob.database.models.ids.OrderStatusId
 
-interface IOrderStatusRepository: ICrudRepository<OrderStatusId, OrderStatusModel, OrderStatusCreateModel, OrderStatusUpdateModel>
+interface IOrderStatusRepository: ICrudRepository<OrderStatusId, OrderStatusModel, OrderStatusCreateModel, OrderStatusUpdateModel> {
+    suspend fun getFull(orderStatusId: OrderStatusId, language: Language): Either<BackendException, OrderStatusFullModel>
+}
