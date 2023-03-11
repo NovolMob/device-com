92f8dab86eff9a8d49de7e5a2c495e4d7cfb396e
NovolMob <60779683+NovolMob@users.noreply.github.com>
2023-03-11 22:23:05 +0300

    feat: uprade versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,10 @@ buildscript {
 plugins {
     id("com.android.library") version "7.3.1" apply false
     id("com.android.application") version "7.3.1" apply false
-    kotlin("multiplatform") version "1.8.0" apply false
+    kotlin("multiplatform") version "1.8.10" apply false
     kotlin("android") version "1.6.10" apply false
-    id("org.jetbrains.compose") version "1.3.0" apply false
-    kotlin("plugin.serialization") version "1.8.0" apply false
+    id("org.jetbrains.compose") version "1.3.1" apply false
+    kotlin("plugin.serialization") version "1.8.10" apply false
 }
 
 subprojects {
